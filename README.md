11662b443bb604b11b921120c19c9ce01436565d
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-12-28 17:53:08 -0400

    added a couple of templates and hooked it up to app.py

--- README.md
+++ README.md
@@ -174,3 +174,19 @@ optional arguments:
  * Debug mode: off
  * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
 ```
+
+### Rendering Templates
+
+```python
+from flask import Flask, render_template
+
+# ...
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+  return render_template('user.html', name=name)
+```
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -9,9 +9,13 @@ def index():
   user_agent = request.headers.get('User-Agent')
   return f"<h1>Hello World!</h1><p>Your browser is {user_agent}</p>"
 
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
 @app.route('/user/<name>')
 def user(name):
-  return f"<p>Hello, {name}!</h1>"
+  return render_template('user.html', name=name)
 
 if __name__ == '__main__':
   manager.run()
